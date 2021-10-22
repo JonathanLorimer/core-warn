1ab6cfb079e23c8bfdac82479db410385ca2dd07
Sandy Maguire <sandy@sandymaguire.me>
2021-10-22 18:23:31 -0400

    reverse lookup and better typesize

--- src/CoercionCheck.hs
+++ src/CoercionCheck.hs
@@ -2,24 +2,28 @@
 
 module CoercionCheck (plugin) where
 
-import Plugins
-import Data.Monoid
-import CoreMonad (CoreToDo(CoreDoPluginPass))
-import HscTypes (ModGuts(ModGuts))
-import CoreSyn (CoreProgram)
-import Outputable (pprPanic)
-import GhcPlugins hiding (singleton, (<>))
-import Data.Generics.Aliases
-import Data.Generics.Schemes
-import CoreStats (coreBindsStats, CoreStats(CS, cs_tm, cs_co), exprStats)
-import Data.Map
-import Data.Foldable hiding (toList)
-import Control.Monad
-import Data.Set (Set)
-import Control.Arrow ((&&&))
+import           Control.Arrow ((&&&))
+import           Control.Monad
+import           CoreMonad (CoreToDo(CoreDoPluginPass))
+import           CoreStats (coreBindsStats, CoreStats(CS, cs_tm, cs_co), exprStats)
+import           CoreSyn (CoreProgram)
+import           Data.Data
+import           Data.Foldable hiding (toList)
+import           Data.Generics.Aliases
+import           Data.Generics.Schemes
+import           Data.Map
+import           Data.Maybe (listToMaybe)
+import           Data.Monoid
+import           Data.Ord
+import           Data.Set (Set)
 import qualified Data.Set as Set
-import Data.Data
-import Data.Ord
+import           GhcPlugins hiding (singleton, (<>), typeSize)
+import           HscTypes (ModGuts(ModGuts))
+import           Outputable (pprPanic)
+import           Plugins
+import           Prelude hiding (lookup)
+import           TcType (tcSplitSigmaTy, tcSplitNestedSigmaTys)
+import           TyCoRep hiding (typeSize)
 
 plugin :: Plugin
 plugin = defaultPlugin
@@ -32,12 +36,18 @@ install ss ctds = pure $ coercionCheck : ctds
 
 coercionCheck :: CoreToDo
 coercionCheck = CoreDoPluginPass "coercionCheck" $ \guts -> do
-  let bindStats = foldMap (fmap exprStats . tabulateBindExpr) $ mg_binds guts
+  let programMap = foldMap tabulateBindExpr $ mg_binds guts
+      bindStats = fmap exprStats programMap
       bindNames = tabulateOccs bindStats
+      derefMap = derefAllVars bindNames programMap
+
   for_ (toList bindNames) \(occName, vars) ->
-    when (heavyOcc vars) $ warnMsg (heavyOccSDoc occName vars)
-  for_ (toList bindStats) \(coreBndr, coreStats) ->
-    when (heavyCoerce coreStats) $ warnMsg (heavyCoerceSDoc coreBndr coreStats)
+    when (heavyOcc vars) $
+      case join $ fmap (listToMaybe . Set.toList) $ lookup occName derefMap of
+        Just name | isGoodSrcSpan (getLoc name)  -> warnMsg (heavyOccSDoc name occName vars)
+        _ -> pure ()
+  -- for_ (toList bindStats) \(coreBndr, coreStats) ->
+  --   when (heavyCoerce coreStats) $ warnMsg (heavyCoerceSDoc coreBndr coreStats)
   pure guts
 
 
@@ -57,25 +67,70 @@ tabulateBindExpr (NonRec var ex) = singleton var ex
 tabulateBindExpr (Rec ex) = foldMap (uncurry singleton) ex
 
 -- Occurence Case
-tabulateOccs :: Map CoreBndr CoreStats -> Map OccName (Set Var)
+tabulateOccs :: Map CoreBndr CoreStats -> Map OccName (Set CoreBndr)
 tabulateOccs = fromListWith (<>)
              . fmap (getOccName &&& Set.singleton)
              . keys
 
-maxTypeSize :: Set Var -> Int
-maxTypeSize = maximum . Set.map (typeSize . idType)
+biggestType :: Set CoreBndr -> Type
+biggestType = rhsType . idType . maximumBy (comparing (typeSize . rhsType . idType))
 
-containsRef :: Data a => Set Name -> a -> Bool
+rhsType :: Type -> Type
+rhsType ty =
+  case tcSplitNestedSigmaTys ty of
+    (_, _, ty') -> ty'
+
+containsRef :: Data a => Set CoreBndr -> a -> Bool
 containsRef names =
   getAny . everything mappend
     (mkQ mempty (Any . flip Set.member names))
 
-heavyOcc :: Set Var -> Bool
-heavyOcc vars = floor (log . fromIntegral $ maxTypeSize vars) < Set.size vars
+heavyOcc :: Set CoreBndr -> Bool
+heavyOcc vars =
+  let occs = Set.size vars
+      biggest = biggestType vars
+      types = typeSize biggest
+   in ceiling (log $ fromIntegral occs) <  types && occs > 1
 
-heavyOccSDoc :: OccName -> Set Var -> SDoc
-heavyOccSDoc name vars = ppr name
-                <+> ppr (getLoc . getName . head . Set.toList $ vars )
-                 $$ ppr (maxTypeSize vars)
-                 $$ ppr (Set.size vars)
-                 $$ text "too many occs"
+heavyOccSDoc :: Name -> OccName -> Set CoreBndr -> SDoc
+heavyOccSDoc ref name vars = ppr name
+                <+> ppr (getLoc ref)
+                 $$ text "type: " <+> ppr (biggestType vars)
+                 $$ text "type size: " <+> ppr (typeSize $ biggestType vars)
+                 $$ text "occ count: " <+> ppr (Set.size vars)
+
+-- whichProgramsContainThisOccNameEquivalenceClass?
+derefVar
+    :: Set CoreBndr           -- equivalence class of names with the same occname
+    -> Map CoreBndr CoreExpr  -- all names to programs
+    -> Set Name               -- names of programs that contain any of the equivalence
+derefVar vars exprs = Set.fromList $ do
+  (bndr, expr) <- toList exprs
+  guard $ containsRef vars expr
+  pure $ getName bndr
+
+-- map from occnames to programs which reference it
+derefAllVars
+    :: Map OccName (Set CoreBndr)
+    -> Map CoreBndr CoreExpr
+    -> Map OccName (Set Name)
+derefAllVars ms exprs = fromList $ do
+  (occ, vars) <- toList ms
+  let x = derefVar vars exprs
+  pure (occ, Set.filter ((/= occ) . getOccName) x)
+
+typeSize :: Type -> Int
+typeSize (LitTy {})               = 1
+typeSize (TyVarTy {})             = 1
+typeSize (AppTy t1 t2)            = typeSize t1 + typeSize t2
+typeSize (FunTy t1 t2)            = typeSize t1 + typeSize t2
+typeSize (ForAllTy (Bndr tv _) t) = typeSize (varType tv) + typeSize t
+typeSize (TyConApp tc [])          =
+  let (kind_vars, _, _) = tcSplitNestedSigmaTys $ tyConKind tc
+   in 1 - length kind_vars
+typeSize (TyConApp tc ts)          =
+  let (kind_vars, _, _) = tcSplitNestedSigmaTys $ tyConKind tc
+   in sum (fmap typeSize ts) - length kind_vars
+typeSize (CastTy ty co)           = typeSize ty + coercionSize co
+typeSize (CoercionTy co)          = 0
+
--- test/InductionTest.hs
+++ test/InductionTest.hs
@@ -9,6 +9,3 @@ import Data.Proxy
 induction :: ()
 induction = requireInductionClass @('[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12]) $ Proxy
 
-
-
-
